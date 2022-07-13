5a24f3b3255db57a6beda3593c58d0c5da3e647e
Hansangjin98 <onetable98@naver.com>
2022-07-14 06:41:05 +0900

    [Feat] 컬러 에셋 및 Extension 구현

---
+++ PlayTogether/PlayTogether/Extensions/UIColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extension.swift
+//  PlayTogether
+//
+//  Created by 한상진 on 2022/07/14.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ptGreen = UIColor(named: "ptGreen") ?? #colorLiteral(red: 0.7725490196, green: 0.9490196078, blue: 0.07450980392, alpha: 1)
+    static let ptBlack01 = UIColor(named: "ptBlack01") ?? #colorLiteral(red: 0.09803921569, green: 0.09803921569, blue: 0.09803921569, alpha: 1)
+    static let ptBlack02 = UIColor(named: "ptBlack02") ?? #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    static let ptGray01 = UIColor(named: "ptGray01") ?? #colorLiteral(red: 0.5098039216, green: 0.5098039216, blue: 0.5098039216, alpha: 1)
+    static let ptGray02 = UIColor(named: "ptGray02") ?? #colorLiteral(red: 0.7725490196, green: 0.7725490196, blue: 0.7725490196, alpha: 1)
+    static let ptGray03 = UIColor(named: "ptGray03") ?? #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)
+    static let ptGray04 = UIColor(named: "ptGray04") ?? #colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 0.73)
+    static let ptIncorrect = UIColor(named: "ptIncorrect") ?? #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+    static let ptCorrect = UIColor(named: "ptCorrect") ?? #colorLiteral(red: 0.231372549, green: 0.8862745098, blue: 0, alpha: 1)
+}
