083c87388b9760d078c1ff3e444740a80dc46fea
sujeong <tnwjd6496@naver.com>
2022-07-23 04:16:33 +0900

    [Feat] PR 리뷰 반영 - layoutSubViews 호출 변경 및 centerY값 생략

--- PlayTogether/PlayTogether/Sources/Thun/View/ThunListTableViewCell.swift
+++ PlayTogether/PlayTogether/Sources/Thun/View/ThunListTableViewCell.swift
@@ -12,25 +12,21 @@ import Then
 class ThunListTableViewCell: UITableViewCell {
     
     static let identifier = "ThunListTableViewCell"
-    
     private let titleLabel = UILabel().then {
         $0.text = "\(ThunDataModel.shared.title)"
         $0.textColor = .ptGreen
-        $0.font = UIFont(name: "Pretendard-Bold", size: 16)
+        $0.font = UIFont.pretendardBold(size: 16)
     }
-    
     private let subTitleLabel = UILabel().then {
         $0.text = "\(ThunDataModel.shared.date) \(ThunDataModel.shared.location) \(ThunDataModel.shared.time)"
         $0.textColor = .white
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        $0.font = UIFont.pretendardSemiBold(size: 14)
     }
-    
     private let personnelLabel = UILabel().then {
         $0.text = "인원 \(ThunDataModel.shared.personnel)"
         $0.textColor = .ptGray02
-        $0.font = UIFont(name: "Pretendard-Medium", size: 12)
+        $0.font = UIFont.pretendardMedium(size: 12)
     }
-
     private let tagLabel = UILabel().then {
         $0.text = " \(ThunDataModel.shared.tag)"
         $0.textColor = .white
@@ -38,26 +34,23 @@ class ThunListTableViewCell: UITableViewCell {
         $0.layer.cornerRadius = 14
         $0.clipsToBounds = true
         $0.textAlignment = .center
-        $0.font = UIFont(name: "Pretendard-Medium", size: 12)
+        $0.font = UIFont.pretendardMedium(size: 12)
     }
-    
     private let likeButton = UIButton().then {
         $0.setImage(UIImage(named: "icn_like_filled_gray"), for: .normal)
         $0.setTitle(" \(ThunDataModel.shared.like)", for: .normal)
-        $0.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 14)
+        $0.titleLabel?.font = UIFont.pretendardMedium(size: 14)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
         setupViews()
         setupLayouts()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
         setupCellStyle()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -70,7 +63,6 @@ class ThunListTableViewCell: UITableViewCell {
         backgroundColor = .clear
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 20, bottom: 10, right: 20))
     }
-    
     private func setupViews() {
         contentView.addSubview(titleLabel)
         contentView.addSubview(subTitleLabel)
@@ -78,33 +70,28 @@ class ThunListTableViewCell: UITableViewCell {
         contentView.addSubview(likeButton)
         contentView.addSubview(tagLabel)
     }
-    
     private func setupLayouts() {
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(16)
             $0.leading.equalToSuperview().offset(20)
         }
-        
         subTitleLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8)
             $0.leading.equalTo(titleLabel.snp.leading)
         }
-        
         personnelLabel.snp.makeConstraints {
             $0.top.equalTo(subTitleLabel.snp.bottom).offset(8)
             $0.leading.equalTo(titleLabel.snp.leading)
             $0.bottom.equalTo(-16)
         }
-        
         tagLabel.snp.makeConstraints {
-            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.centerY.equalTo(titleLabel)
             $0.trailing.equalToSuperview().offset(-20)
             $0.size.equalTo(CGSize(width: 52, height: 26))
         }
-        
         likeButton.snp.makeConstraints {
             $0.trailing.equalTo(tagLabel.snp.trailing)
-            $0.centerY.equalTo(personnelLabel.snp.centerY)
+            $0.centerY.equalTo(personnelLabel)
         }
     }
 }
