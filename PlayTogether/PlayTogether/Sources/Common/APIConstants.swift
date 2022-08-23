d4d7b748d1b1943b635d57a87ae0c9de0c6d7db8
sujeong <tnwjd6496@naver.com>
2022-08-23 19:28:30 +0900

    [Feat] APIConstants 추가

--- PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
+++ PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
@@ -12,17 +12,21 @@ struct APIConstants {
     
     //TODO: 추후 삭제 예정
     static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxIiwidXNlckxvZ2luSWQiOiJpb3NUZXN0IiwiaWF0IjoxNjU4ODU2OTEwLCJleHAiOjE2NjE0NDg5MTAsImlzcyI6InBsYXl0b2dldGhlciJ9.dtWSxC8zZNQlnffughIdjMiA6wFNO3zUJ476pfoUtvA"
+    static let crewID = 102
     
     static let subwayServiceKey = "Gd9kQKfYBRl%2Bfk8CBPW9CHL5ZFvQUWQiIR6yQ%2F84qRh8HclgviJO9lCOI%2BRHmz%2BPZ9FMdjJWgzn31z8MmVNN2g%3D%3D"
     
     // light
     static let getHotThunList = "/light/hot"
     static let getNewThunList = "/light/new"
-    static let getSubmittedThunList = "/light/enter"
-    static let getOpenedThunList = "/light/open"
-    static let getLikedThunList = "/light/scrap"
     static let existingNickname = "/user/crew"
     
     // subway
     static let getStationList = "/getKwrdFndSubwaySttnList"
+    static let getSubmittedThunList = "/light"
+    static let getOpenedThunList = "/light"
+    static let getLikedThunList = "/light"
+    static let getDetailThunList = "/light/detail"
+    
+    static let postDetailThunCancel = "/light/enter"
 }
