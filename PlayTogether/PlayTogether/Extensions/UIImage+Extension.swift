2ac94128241facbca59e3aa17dbb7ba0e257e522
sujeong <tnwjd6496@naver.com>
2022-08-23 19:26:25 +0900

    [Feat] 이미지 추가

--- PlayTogether/PlayTogether/Extensions/UIImage+Extension.swift
+++ PlayTogether/PlayTogether/Extensions/UIImage+Extension.swift
@@ -42,4 +42,14 @@ enum ImageCase: String {
     
     case beforeIcon = "beforeIcon"
     case afterIcon = "afterIcon"
+    
+    case profileIcon = "profileIcon"
+    
+    case likeFilledGrayIcon = "likeFilledGrayIcon"
+    case likeFilledGreenIcon = "likeFilledGreenIcon"
+    
+    case navLikeDefaultIcon = "navLikeDefaultIcon"
+    case navLikeFilledGreenIcon = "navLikeFilledGreenIcon"
+    
+    case arrowRightIcon = "arrowRightIcon"
 }
