1c4b1e362e04c0b8882f03703218641b67fc99e5
sujeong <tnwjd6496@naver.com>
2022-08-24 05:32:27 +0900

    [Feat] 번개 탭 뷰 수정

--- PlayTogether/PlayTogether/Sources/Thun/View/OpenedThunViewController.swift
+++ PlayTogether/PlayTogether/Sources/Thun/View/OpenedThunViewController.swift
@@ -13,10 +13,22 @@ import RxSwift
 class OpenedThunViewController: BaseViewController {
     
     private lazy var disposeBag = DisposeBag()
-    private let viewModel = ThunViewModel()
+    private let viewModel: ThunViewModel?
+    
+    init(viewModel: ThunViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     private lazy var tableView = UITableView().then {
-        $0.register(ThunListTableViewCell.self, forCellReuseIdentifier: ThunListTableViewCell.identifier)
+        $0.register(
+            ThunListTableViewCell.self,
+            forCellReuseIdentifier: ThunListTableViewCell.identifier
+        )
         $0.separatorStyle = .none
         $0.showsVerticalScrollIndicator = false
         $0.rowHeight = 110
@@ -36,7 +48,7 @@ class OpenedThunViewController: BaseViewController {
     }
     
     override func setupBinding() {
-        viewModel.openedThunList
+        viewModel?.openedThunList
                .bind(to: self.tableView.rx.items) { _, row, item -> UITableViewCell in
                    guard let cell = self.tableView.dequeueReusableCell(
                        withIdentifier: "ThunListTableViewCell",
@@ -45,7 +57,16 @@ class OpenedThunViewController: BaseViewController {
                          let item = item
                    else { return UITableViewCell() }
                    
-                   cell.setupData(item.title, item.date, item.time, item.peopleCnt, item.place, item.lightMemberCnt, item.category, item.scpCnt)
+                   cell.setupData(
+                    item.title,
+                    item.date ?? "",
+                    item.time ?? "",
+                    item.peopleCnt ?? 0,
+                    item.place ?? "",
+                    item.lightMemberCnt,
+                    item.category,
+                    item.scpCnt
+                   )
                    return cell
                }
                .disposed(by: disposeBag)
