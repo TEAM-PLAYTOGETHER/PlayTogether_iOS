6087729ff55e4c01f497bb4cfbe0d7c2e6bc3e0e
sujeong <tnwjd6496@naver.com>
2022-08-23 19:28:30 +0900

    [Feat] APIConstants 추가

--- PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
+++ PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
@@ -11,11 +11,15 @@ struct APIConstants {
     
     //TODO: 추후 삭제 예정
     static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxIiwidXNlckxvZ2luSWQiOiJpb3NUZXN0IiwiaWF0IjoxNjU4ODU2OTEwLCJleHAiOjE2NjE0NDg5MTAsImlzcyI6InBsYXl0b2dldGhlciJ9.dtWSxC8zZNQlnffughIdjMiA6wFNO3zUJ476pfoUtvA"
+    static let crewID = 102
     
     // light
     static let getHotThunList = "/light/hot"
     static let getNewThunList = "/light/new"
-    static let getSubmittedThunList = "/light/enter"
-    static let getOpenedThunList = "/light/open"
-    static let getLikedThunList = "/light/scrap"
+    static let getSubmittedThunList = "/light"
+    static let getOpenedThunList = "/light"
+    static let getLikedThunList = "/light"
+    static let getDetailThunList = "/light/detail"
+    
+    static let postDetailThunCancel = "/light/enter"
 }
