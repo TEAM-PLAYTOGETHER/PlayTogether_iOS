b680dd4dbaf7113d1cbaf4dab2ad6207755b2392
sujeong <tnwjd6496@naver.com>
2022-08-27 17:01:47 +0900

    [Feat] 토큰 및 crewId 변경

--- PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
+++ PlayTogether/PlayTogether/Sources/Common/APIConstants.swift
@@ -11,22 +11,21 @@ struct APIConstants {
     static let subwayBaseUrl = "http://apis.data.go.kr/1613000/SubwayInfoService"
     
     //TODO: 추후 삭제 예정
-    static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxIiwidXNlckxvZ2luSWQiOiJpb3NUZXN0IiwiaWF0IjoxNjU4ODU2OTEwLCJleHAiOjE2NjE0NDg5MTAsImlzcyI6InBsYXl0b2dldGhlciJ9.dtWSxC8zZNQlnffughIdjMiA6wFNO3zUJ476pfoUtvA"
-    static let crewID = 102
+    static let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgzIiwidXNlckxvZ2luSWQiOiJkbHdwd25zMDIiLCJpYXQiOjE2NTkzMzcyMzgsImV4cCI6MTY2MTkyOTIzOCwiaXNzIjoicGxheXRvZ2V0aGVyIn0.SxPA4KTBzKFe7nTVb3lQecUFxaRnMogOutB072Mzb8Y"
+    static let crewID = 56
     
     static let subwayServiceKey = "Gd9kQKfYBRl%2Bfk8CBPW9CHL5ZFvQUWQiIR6yQ%2F84qRh8HclgviJO9lCOI%2BRHmz%2BPZ9FMdjJWgzn31z8MmVNN2g%3D%3D"
     
     // light
     static let getHotThunList = "/light/hot"
     static let getNewThunList = "/light/new"
-    static let existingNickname = "/user/crew"
-    
-    // subway
-    static let getStationList = "/getKwrdFndSubwaySttnList"
     static let getSubmittedThunList = "/light"
     static let getOpenedThunList = "/light"
     static let getLikedThunList = "/light"
     static let getDetailThunList = "/light/detail"
-    
     static let postDetailThunCancel = "/light/enter"
+    static let existingNickname = "/user/crew"
+    
+    // subway
+    static let getStationList = "/getKwrdFndSubwaySttnList"
 }
