3f39f45b6dead7c0ab02ef638d72a5f195c9d89a
sujeong <tnwjd6496@naver.com>
2022-08-27 17:04:34 +0900

    [Feat] 줄바꿈 변경

--- PlayTogether/PlayTogether/Sources/Thun/View/UnderlineSegmentedControl.swift
+++ PlayTogether/PlayTogether/Sources/Thun/View/UnderlineSegmentedControl.swift
@@ -10,7 +10,12 @@ import SnapKit
 import Then
 
 class UnderlineSegmentedControl: UISegmentedControl {
-    private lazy var underlineView = UIView(frame: CGRect(x: CGFloat(self.selectedSegmentIndex * Int(bounds.size.width/CGFloat(numberOfSegments))), y: bounds.size.height - 3.0, width: bounds.size.width/CGFloat(numberOfSegments), height: 5.0)).then {
+    private lazy var underlineView = UIView(frame: CGRect(
+        x: CGFloat(self.selectedSegmentIndex * Int(bounds.size.width/CGFloat(numberOfSegments))),
+        y: bounds.size.height - 3.0,
+        width: bounds.size.width/CGFloat(numberOfSegments),
+        height: 5.0)
+    ).then {
         $0.backgroundColor = UIColor.ptGreen
         addSubview($0)
     }
