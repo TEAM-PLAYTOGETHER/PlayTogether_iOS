b96569e6ae134fee2865c8a7c34ffd3054bc7e52
sujeong <tnwjd6496@naver.com>
2022-08-30 22:41:50 +0900

    [Feat] 특정 글자 색 변경하는 extension 수정

--- PlayTogether/PlayTogether/Extensions/UILabel+Extension.swift
+++ PlayTogether/PlayTogether/Extensions/UILabel+Extension.swift
@@ -17,12 +17,11 @@ extension UILabel {
     }
     
     /// 특정 글자색 변경
-    func changeFontColor(targetString: String) {
+    func changeFontColor(targetString: String, color: UIColor) {
         let fullText = text ?? ""
         let attributedString = NSMutableAttributedString(string: fullText)
         let range = (fullText as NSString).range(of: targetString)
         let font = UIFont.pretendardRegular(size: 14)
-        let color = UIColor.ptGreen
         attributedString.addAttributes([.font: font, .foregroundColor: color], range: range)
         attributedText = attributedString
     }
