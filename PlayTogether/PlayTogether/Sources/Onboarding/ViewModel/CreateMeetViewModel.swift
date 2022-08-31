eca5a134c8b22222d34db407181f9936a96b8bea
Jiseok97 <wltjr1426@naver.com>
2022-08-31 22:32:42 +0900

    [Feat] 온보딩 동아리 개설 뷰 수정

--- PlayTogether/PlayTogether/Sources/Onboarding/ViewModel/CreateMeetViewModel.swift
+++ PlayTogether/PlayTogether/Sources/Onboarding/ViewModel/CreateMeetViewModel.swift
@@ -11,38 +11,17 @@ import RxCocoa
 final class CreateMeetViewModel {
     
     struct Input {
-        var meetingTitleText: Observable<String>
+        var checkMeetingTitle: Observable<Bool>
         var introduceText: Observable<String>
-    }
-    
-    struct RegularExpressionInput {
-        var meetingTitleText: Observable<String>
-    }
-    
-    struct RegularExpressionOutput {
-        var titleTextCheck: Driver<Bool>
     }
     
     struct nextButtonEnableOutput {
         var nextButtonEnableCheck: Driver<Bool>
     }
     
-    func regularExpressionCheck(input: RegularExpressionInput) -> RegularExpressionOutput {
-        let output = input.meetingTitleText.map {
-            let pattern = "^[0-9a-zㅏ-ㅣA-Zㄱ-ㅎ가-핳\\s]*$"
-            guard let _ = $0.range(of: pattern, options: .regularExpression) else { return false }
-
-            return true
-        }.asDriver(onErrorJustReturn: false)
-        
-        return RegularExpressionOutput(titleTextCheck: output)
-    }
-    
     func isNextButtonEnable(input: Input) -> nextButtonEnableOutput {
-        let output = Observable.combineLatest(input.meetingTitleText, input.introduceText) {
-            let pattern = "^[0-9a-zㅏ-ㅣA-Zㄱ-ㅎ가-핳\\s]*$"
-            guard let _ = $0.range(of: pattern, options: .regularExpression) else { return false }
-            return !$0.isEmpty && !$1.isEmpty
+        let output = Observable.combineLatest(input.checkMeetingTitle, input.introduceText) {
+            return $0 && !$1.isEmpty
         }.asDriver(onErrorJustReturn: false)
         
         return nextButtonEnableOutput(nextButtonEnableCheck: output)
