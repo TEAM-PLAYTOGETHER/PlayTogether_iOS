bd6e7fae778d628854131c93cb08c6e4ff8726d5
Jiseok97 <wltjr1426@naver.com>
2022-09-26 10:32:00 +0900

    [Remove] 연관 없는 파일 제거

--- PlayTogether/PlayTogether/Sources/TermsService/Network/AddUserInfoResponse.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AddUserInfoResponse.swift
-//  PlayTogether
-//
-//  Created by 이지석 on 2022/09/06.
-//
-
-import Foundation
--- PlayTogether/PlayTogether/Sources/TermsService/ViewModel/InputGenderBirthYearViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  InputGenderBirthYearViewModel.swift
-//  PlayTogether
-//
-//  Created by 이지석 on 2022/09/06.
-//
-
-import Foundation
