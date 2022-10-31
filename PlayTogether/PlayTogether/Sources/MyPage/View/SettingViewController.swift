8562a38b5546f82a59c666fbe8a4ba50992b8b81
sujeong <tnwjd6496@naver.com>
2022-10-31 15:18:00 +0900

    [Feat] 설정 뷰 UI 구현 및 계정 관리 뷰와 연결

--- PlayTogether/PlayTogether/Sources/MyPage/View/SettingViewController.swift
+++ PlayTogether/PlayTogether/Sources/MyPage/View/SettingViewController.swift
@@ -7,20 +7,28 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class SettingViewController: BaseViewController {
     private let disposeBag = DisposeBag()
-    private let viewModel = MyPageViewModel()
+    private let viewModel = SettingViewModel()
     
     private let backButton = UIButton().then {
         $0.setImage(.ptImage(.backIcon), for: .normal)
     }
     
     private lazy var tableView = UITableView().then {
-        $0.bounces = false
         $0.rowHeight = UIScreen.main.bounds.height * 0.061
         $0.register(MyPageMenuTableViewCell.self, forCellReuseIdentifier: "MyPageMenuTableViewCell")
-        $0.backgroundColor = .red
+        $0.delegate = self
+        $0.dataSource = self
+        $0.separatorStyle = .none
+        $0.sectionHeaderTopPadding = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = false
     }
 
     override func setupViews() {
@@ -43,28 +51,57 @@ class SettingViewController: BaseViewController {
                 self.navigationController?.popViewController(animated: true)
             })
             .disposed(by: disposeBag)
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 2
+        case 1: return 4
+        case 2: return 1
+        default: break
+        }
+        return Int()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "MyPageMenuTableViewCell",
+            for: indexPath
+        ) as? MyPageMenuTableViewCell else { return UITableViewCell() }
         
-        Observable.just(viewModel.menuList)
-            .bind(to: tableView.rx.items) { _, row, item -> UITableViewCell in
-                guard let cell = self.tableView.dequeueReusableCell(
-                    withIdentifier: "MyPageMenuTableViewCell",
-                    for: IndexPath(row: row, section: 0)
-                ) as? MyPageMenuTableViewCell else { return UITableViewCell() }
-                
-                cell.setupCell(menuName: item)
-                
-//                guard row == self.viewModel.menuList.count - 1
-//                else { return cell }
-//
-//                let halfWidth = self.view.frame.width / 2
-//                cell.separatorInset = UIEdgeInsets(
-//                    top: 0,
-//                    left: halfWidth,
-//                    bottom: 0,
-//                    right: halfWidth
-//                )
-                return cell
+        switch indexPath.section {
+        case 0: cell.setupCell(menuName: viewModel.firstMenuList[indexPath.row])
+        case 1: cell.setupCell(menuName: viewModel.secondMenuList[indexPath.row])
+        case 2: cell.setupCell(menuName: viewModel.thirdMenuList[indexPath.row])
+        default: break
+        }
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .init(red: 235/255, green: 235/255, blue: 235/255, alpha: 1)
+
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section != 0 ? 5 : 0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 1 {
+                navigationController?.pushViewController(ManageAccountViewController(), animated: true)
             }
-            .disposed(by: self.disposeBag)
+        default: break
+        }
     }
 }
