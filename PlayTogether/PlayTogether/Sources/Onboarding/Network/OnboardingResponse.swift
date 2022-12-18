c05a85d894142e0a08669f3efaf142d03115421b
Jiseok97 <wltjr1426@naver.com>
2022-12-18 11:33:32 +0900

    [Feat] Onboarding ViewModel 함수 추가

--- PlayTogether/PlayTogether/Sources/Onboarding/Network/OnboardingResponse.swift
+++ PlayTogether/PlayTogether/Sources/Onboarding/Network/OnboardingResponse.swift
@@ -5,4 +5,25 @@
 //  Created by 이지석 on 2022/12/18.
 //
 
-import Foundation
+struct OnboardingResponse: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: CrewData
+}
+
+struct CrewData: Codable {
+    let list: [CrewLists]
+}
+
+struct CrewLists: Codable {
+    let id: Int
+    let crewCode, name, listDescription: String
+    let isAdmin: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id, crewCode, name
+        case listDescription = "description"
+        case isAdmin
+    }
+}
--- PlayTogether/PlayTogether/Sources/Onboarding/ViewModel/OnboardingViewModel.swift
+++ PlayTogether/PlayTogether/Sources/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -13,10 +13,23 @@ struct OnboardingViewModel {
     private let provider = MoyaProvider<OnboardingService>()
     
     struct UserJWTToken {
-        var token: Observable<String>
+        var token: String
     }
     
-    func getCrewList() {
-        
+    func getCrewList(jwt: UserJWTToken, completion: @escaping (CrewLists) -> Void) {
+        provider.rx.request(.getCrewInfo(
+            jwtToken: jwt.token
+        ))
+        .subscribe{ result in
+            switch result {
+            case .success(let response):
+                guard let responseData = try? response.map(CrewLists.self) else { return }
+                completion(responseData)
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        .disposed(by: disposeBag)
     }
 }
