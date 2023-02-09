d87a5597f5b529e62699985b49a18119bb553475
이지석 <jiseok2301@newlinkcorp.co.kr>
2023-02-09 11:47:27 +0900

    [Add] 동아리 생성(CreateMeetService) 네트워크 파일 생성

---
+++ PlayTogether/PlayTogether/Sources/Onboarding/Network/CreateMeetService.swift
@@ -0,0 +1,49 @@
+//
+//  CreateMeetService.swift
+//  PlayTogether
+//
+//  Created by 이지석 on 2023/02/09.
+//
+
+import Moya
+import Foundation
+
+enum CreateMeetService {
+    case createMeetRequest(crewName: String, description: String, jwt: String)
+}
+
+extension CreateMeetService: TargetType {
+    var baseURL: URL {
+        return URL(string: APIConstants.baseUrl)!
+    }
+    
+    var path: String {
+        return APIConstants.postCreateMeet
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var task: Task {
+        switch self {
+        case .createMeetRequest(let crewName, let description, _):
+            let param: [String : Any] = [
+                "crewName": crewName,
+                "description": description
+            ]
+            return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+        }
+        
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .createMeetRequest(_, _, let jwt):
+            return [
+                "Content-Type": "application/json",
+                "Authorization": jwt
+            ]
+        }
+    }
+}
